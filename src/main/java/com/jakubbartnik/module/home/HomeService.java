5a9b9e2f5defe3272aa47adc8f05097a3a32f90c
Jakub Bartnik <bartnik212@gmail.com>
2020-10-03 10:03:16 +0200

    add HomeController, HomeService, home.html

---
+++ src/main/java/com/jakubbartnik/module/home/HomeService.java
@@ -0,0 +1,11 @@
+package com.jakubbartnik.module.home;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public String getGreetings() {
+        return "Hello world";
+    }
+}
