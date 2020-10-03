54bf1eda9fa18cc5c00810403145178b79c77aae
Jakub Bartnik <bartnik212@gmail.com>
2020-10-03 13:26:00 +0200

    add BooksController, BooksEntity

---
+++ src/main/java/com/jakubbartnik/module/books/BooksController.java
@@ -0,0 +1,34 @@
+package com.jakubbartnik.module.books;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class BooksController {
+
+    private static final List<BooksEntity> BOOKS_ENTITIES = List.of(
+            new BooksEntity()
+            .setId(1L).setTitle("The Lord of The Rings").setAuthor("J.R.R Tolkien"),
+            new BooksEntity()
+            .setId(2L).setTitle("1984").setAuthor("George Orwell"),
+            new BooksEntity()
+            .setId(3L).setTitle("Galaxy").setAuthor("Douglas Adams")
+    );
+
+    @GetMapping("/rest/books/{id}")
+    public BooksEntity getBook(@PathVariable Long id){
+        return BOOKS_ENTITIES
+                .stream()
+                .filter(book -> book.getId().equals(id))
+                .findFirst()
+                .get();
+    }
+    @GetMapping("/rest/books")
+    public List<BooksEntity> getBooks(){
+        return BOOKS_ENTITIES;
+    }
+
+}
---
+++ src/main/java/com/jakubbartnik/module/books/BooksEntity.java
@@ -0,0 +1,37 @@
+package com.jakubbartnik.module.books;
+
+
+public class BooksEntity {
+    private Long id;
+    private String author;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BooksEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public BooksEntity setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public BooksEntity setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+
+}
