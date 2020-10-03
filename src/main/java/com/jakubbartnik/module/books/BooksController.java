db3005f725a964672e2c8914b95641d98525d28c
Jakub Bartnik <bartnik212@gmail.com>
2020-10-03 14:38:53 +0200

    update BooksController

--- src/main/java/com/jakubbartnik/module/books/BooksController.java
+++ src/main/java/com/jakubbartnik/module/books/BooksController.java
@@ -1,9 +1,7 @@
 package com.jakubbartnik.module.books;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,11 @@ public class BooksController {
     @GetMapping("/rest/books")
     public List<BooksEntity> getBooks() {
         return booksRepository.findAll();
+    }
+
+    @PostMapping("/rest/books")
+    public BooksEntity newBooks(@RequestBody BooksEntity booksEntity){
+        return booksRepository.saveAndFlush(booksEntity);
     }
 
 }
