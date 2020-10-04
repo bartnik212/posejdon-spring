b96b3d941d8f2fc22e43a7023917332142464a58
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 11:20:31 +0200

    refactoring BooksController

--- src/main/java/com/jakubbartnik/module/books/BooksController.java
+++ src/main/java/com/jakubbartnik/module/books/BooksController.java
@@ -1,6 +1,8 @@
 package com.jakubbartnik.module.books;
 
+import com.jakubbartnik.module.books.dto.BookDto;
 import com.jakubbartnik.module.books.entity.BooksEntity;
+import com.jakubbartnik.module.books.mapper.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,34 +12,37 @@ import java.util.List;
 @RestController
 public class BooksController {
 
-
-
     @Autowired
     private BooksRepository booksRepository;
 
     @GetMapping("/rest/books/{id}")
-    public BooksEntity getBook(@PathVariable Long id){
-       return booksRepository.findById(id).orElse(null);
+    public BookDto getBook(@PathVariable Long id) {
+        BooksEntity booksEntity = booksRepository.findById(id).orElse(null);
+        return BookMapper.map(booksEntity);
     }
+
     @GetMapping("/rest/books")
-    public List<BooksEntity> getBooks() {
-        return booksRepository.findAll();
+    public List<BookDto> getBooks() {
+        List<BooksEntity> books = booksRepository.findAll();
+        return BookMapper.map(books);
     }
 
     @PostMapping("/rest/books")
-    public BooksEntity newBooks(@Valid @RequestBody BooksEntity booksEntity){
-        return booksRepository.saveAndFlush(booksEntity);
+    public BookDto newBooks(@Valid @RequestBody BooksEntity booksEntity) {
+        BooksEntity book = booksRepository.saveAndFlush(booksEntity);
+        return BookMapper.map(book);
+    }
+
+    @PutMapping("/rest/books/{id}")
+    public BookDto updateBooks(@PathVariable Long id,
+                               @RequestBody BooksEntity booksEntity) {
+        BooksEntity book = booksRepository.saveAndFlush(booksEntity.setId(id));
+        return BookMapper.map(book);
     }
 
     @DeleteMapping("/rest/books/{id}")
     public void delete(@PathVariable Long id) {
         booksRepository.deleteById(id);
-    }
-
-    @PutMapping("/rest/books/{id}")
-    public BooksEntity updateBooks (@PathVariable Long id,
-                                    @RequestBody BooksEntity booksEntity){
-        return booksRepository.saveAndFlush(booksEntity.setId(id));
     }
 
 }
