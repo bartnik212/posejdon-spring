6e1b804e5aad9978270deabf1030e72c24785778
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 15:06:33 +0200

    update BookFromMapper

--- src/main/java/com/jakubbartnik/module/books/mapper/BookFormMapper.java
+++ src/main/java/com/jakubbartnik/module/books/mapper/BookFormMapper.java
@@ -3,20 +3,37 @@ package com.jakubbartnik.module.books.mapper;
 import com.jakubbartnik.module.books.dto.BookForm;
 import com.jakubbartnik.module.books.entity.BooksDetailsEntity;
 import com.jakubbartnik.module.books.entity.BooksEntity;
+import com.jakubbartnik.module.books.entity.BooksTagsEntity;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BookFormMapper {
 
     public static BooksEntity map(BookForm form) {
-        return new BooksEntity()
-                .setTitle(form.getTitle())
+        BooksEntity entity = new BooksEntity();
+        entity.setTitle(form.getTitle())
                 .setAuthor(form.getAuthor())
-                .setDetails(getDetails(form));
+                .setDetails(getDetails(form))
+                .setTags(getTags(form, entity));
+        return entity;
     }
+
 
     private static BooksDetailsEntity getDetails(BookForm form) {
 
         return new BooksDetailsEntity()
                 .setIsbn(form.getIsbn())
                 .setLang(form.getLang());
+    }
+
+    private static Set<BooksTagsEntity> getTags(BookForm form, BooksEntity entity) {
+        return form
+                .getTags()
+                .stream()
+                .map(tag -> new BooksTagsEntity()
+                        .setValue(tag)
+                        .setBook(entity))
+                .collect(Collectors.toSet());
     }
 }
