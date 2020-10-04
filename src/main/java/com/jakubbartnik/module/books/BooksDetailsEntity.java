5d8129c7325d68113d8491493e9c8cf1df5d0cba
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 09:46:37 +0200

    add BooksDetailsEntity

---
+++ src/main/java/com/jakubbartnik/module/books/BooksDetailsEntity.java
@@ -0,0 +1,28 @@
+package com.jakubbartnik.module.books;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books_details")
+public class BooksDetailsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+    private String lang;
+    private String publisher;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+}
