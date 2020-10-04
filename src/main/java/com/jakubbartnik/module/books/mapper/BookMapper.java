11baaee49f3cc94f6e5a7a6b2fd0d3d046f8029d
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 11:10:56 +0200

    add BookMapper

---
+++ src/main/java/com/jakubbartnik/module/books/mapper/BookMapper.java
@@ -0,0 +1,23 @@
+package com.jakubbartnik.module.books.mapper;
+
+import com.jakubbartnik.module.books.dto.BookDto;
+import com.jakubbartnik.module.books.entity.BooksEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BookMapper {
+
+    public static BookDto map(BooksEntity entity){
+        return new BookDto()
+                .setAuthor(entity.getAuthor())
+                .setTitle(entity.getTitle());
+    }
+
+    public static List<BookDto> map(List<BooksEntity> entities){
+        return entities
+                .stream()
+                .map(BookMapper::map)
+                .collect(Collectors.toList());
+    }
+}
