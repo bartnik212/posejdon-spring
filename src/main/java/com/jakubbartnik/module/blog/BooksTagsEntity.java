87fd275da544e5cdd77c8ffe889f8b1f90cdaac8
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 10:20:56 +0200

    add BooksTagsEntity

---
+++ src/main/java/com/jakubbartnik/module/blog/BooksTagsEntity.java
@@ -0,0 +1,40 @@
+package com.jakubbartnik.module.blog;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.jakubbartnik.module.books.BooksEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books_tags")
+@JsonIgnoreProperties("book")
+public class BooksTagsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private BooksEntity book;
+
+    private String value;
+
+    public BooksEntity getBook() {
+        return book;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public BooksTagsEntity setBook(BooksEntity book) {
+        this.book = book;
+        return this;
+    }
+
+    public BooksTagsEntity setValue(String value) {
+        this.value = value;
+        return this;
+    }
+}
