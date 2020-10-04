a94f64aba20496c2a358d5f7a6218beba3b104b7
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 13:33:35 +0200

    add BooksTagsMapper

---
+++ src/main/java/com/jakubbartnik/module/books/mapper/BooksTagsMapper.java
@@ -0,0 +1,24 @@
+package com.jakubbartnik.module.books.mapper;
+
+import com.jakubbartnik.module.books.entity.BooksTagsEntity;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BooksTagsMapper {
+
+    public static String map(BooksTagsEntity tag) {
+        return tag.getValue();
+    }
+
+    public static Set<String> map(Set<BooksTagsEntity> tags) {
+        if (tags == null) {
+            return Set.of();
+        }
+
+        return tags
+                .stream()
+                .map(BooksTagsMapper::map)
+                .collect(Collectors.toSet());
+    }
+}
