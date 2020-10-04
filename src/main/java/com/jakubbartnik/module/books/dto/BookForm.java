467a4d541d4e1417a126c6913d5585629e63dd7d
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 15:02:19 +0200

    update BookForm

--- src/main/java/com/jakubbartnik/module/books/dto/BookForm.java
+++ src/main/java/com/jakubbartnik/module/books/dto/BookForm.java
@@ -1,11 +1,19 @@
 package com.jakubbartnik.module.books.dto;
 
+import java.util.List;
+
 public class BookForm {
 
     private String author;
     private String title;
     private String isbn;
     private String lang;
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    private List<String> tags;
 
     public String getIsbn() {
         return isbn;
