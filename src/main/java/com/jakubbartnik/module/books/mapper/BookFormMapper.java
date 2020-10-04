12f5c61924054a1f1691b5a87451413d3feb5d59
Jakub Bartnik <bartnik212@gmail.com>
2020-10-04 14:47:14 +0200

    update BookFormMapper

--- src/main/java/com/jakubbartnik/module/books/mapper/BookFormMapper.java
+++ src/main/java/com/jakubbartnik/module/books/mapper/BookFormMapper.java
@@ -1,6 +1,7 @@
 package com.jakubbartnik.module.books.mapper;
 
 import com.jakubbartnik.module.books.dto.BookForm;
+import com.jakubbartnik.module.books.entity.BooksDetailsEntity;
 import com.jakubbartnik.module.books.entity.BooksEntity;
 
 public class BookFormMapper {
@@ -8,6 +9,14 @@ public class BookFormMapper {
     public static BooksEntity map(BookForm form) {
         return new BooksEntity()
                 .setTitle(form.getTitle())
-                .setAuthor(form.getAuthor());
+                .setAuthor(form.getAuthor())
+                .setDetails(getDetails(form));
+    }
+
+    private static BooksDetailsEntity getDetails(BookForm form) {
+
+        return new BooksDetailsEntity()
+                .setIsbn(form.getIsbn())
+                .setLang(form.getLang());
     }
 }
